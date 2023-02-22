a5ae54903fb1add5b305e7f9989925304809c190
PROGRAMADOR RD <jhamidridiaz@gmail.com>
2023-02-22 01:52:12 -0500

    Quiz Application | Android Studio

---
+++ app/src/main/java/com/example/preparateya/MainActivity.java
@@ -0,0 +1,117 @@
+package com.example.preparateya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+
+    TextView totalQuestionsTextView;
+    TextView questionTextView;
+    Button ansA, ansB, ansC;
+    Button submitBtn;
+
+    int score=0;
+    int totalQuestion = PreguntaRespuesta.question.length;
+    int currentQuestionIndex = 0;
+    String selectedAnswer = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        totalQuestionsTextView = findViewById(R.id.total_question);
+        questionTextView = findViewById(R.id.question);
+        ansA = findViewById(R.id.ans_A);
+        ansB = findViewById(R.id.ans_B);
+        ansC = findViewById(R.id.ans_C);
+
+        submitBtn = findViewById(R.id.submit_btn);
+
+        ansA.setOnClickListener(this);
+        ansB.setOnClickListener(this);
+        ansC.setOnClickListener(this);
+
+        submitBtn.setOnClickListener(this);
+
+        totalQuestionsTextView.setText("Total questions : "+totalQuestion);
+
+        loadNewQuestion();
+
+
+
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        ansA.setBackgroundColor(Color.WHITE);
+        ansB.setBackgroundColor(Color.WHITE);
+        ansC.setBackgroundColor(Color.WHITE);
+
+
+        Button clickedButton = (Button) view;
+        if(clickedButton.getId()==R.id.submit_btn){
+            if(selectedAnswer.equals(PreguntaRespuesta.correctAnswers[currentQuestionIndex])){
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }else{
+            //choices button clicked
+            selectedAnswer  = clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.MAGENTA);
+
+        }
+
+    }
+
+    void loadNewQuestion(){
+
+        if(currentQuestionIndex == totalQuestion ){
+            finishQuiz();
+            return;
+        }
+
+        questionTextView.setText(PreguntaRespuesta.question[currentQuestionIndex]);
+        ansA.setText(PreguntaRespuesta.choices[currentQuestionIndex][0]);
+        ansB.setText(PreguntaRespuesta.choices[currentQuestionIndex][1]);
+        ansC.setText(PreguntaRespuesta.choices[currentQuestionIndex][2]);
+
+
+    }
+
+    void finishQuiz(){
+        String passStatus = "";
+        if(score > totalQuestion*0.60){
+            passStatus = "Passed";
+        }else{
+            passStatus = "Failed";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is "+ score+" out of "+ totalQuestion)
+                .setPositiveButton("Restart",(dialogInterface, i) -> restartQuiz() )
+                .setCancelable(false)
+                .show();
+
+
+    }
+
+    void restartQuiz(){
+        score = 0;
+        currentQuestionIndex =0;
+        loadNewQuestion();
+    }
+}
---
+++ app/src/main/java/com/example/preparateya/PreguntaRespuesta.java
@@ -0,0 +1,87 @@
+package com.example.preparateya;
+
+public class PreguntaRespuesta {
+    public static String question[]={
+
+            "1. ¿Qué es Spring?",
+            "2. ¿Qué es Spring Boot?",
+            "3. ¿Qué es Inversión de Control (IoC) en Spring?",
+            "4. ¿Qué es Dependency Injection (DI) en Spring?",
+            "5. ¿Qué es un bean en Spring?",
+            "6. ¿Cuál es el archivo de configuración principal de Spring?",
+            "7. ¿Qué es un contenedor de IoC en Spring?",
+            "8. ¿Qué es un controlador en Spring MVC?",
+            "9. ¿Qué es un servicio en Spring?",
+            "10. ¿Qué es una vista en Spring MVC?",
+            "11. ¿Qué es el archivo application.properties en Spring Boot?",
+            "12. ¿Qué es la anotación @Autowired en Spring?",
+            "13. ¿Qué es la anotación @RestController en Spring MVC?",
+            "14. ¿Qué es la anotación @Service en Spring?",
+            "15. ¿Qué es la anotación @Component en Spring?",
+            "16. ¿Qué es la anotación @Value en Spring?",
+            "17. ¿Qué es la anotación @RequestMapping en Spring MVC?",
+            "18. ¿Qué es el contenedor de IoC en Spring?",
+            "19. ¿Qué es un Bean en Spring?",
+            "20. ¿Qué es la anotación @Transactional en Spring?"
+
+
+    };
+    public static String choices[][]={
+            {"a) Un lenguaje de programación","b) Un framework de desarrollo de aplicaciones","c) Una base de datos"},
+            {"a) Un proyecto que se encarga de la configuración inicial de una aplicación Spring","b) Un framework que se utiliza para crear aplicaciones móviles","c) Un módulo de Spring que se encarga de la gestión de transacciones"},
+            {"a) Un patrón de diseño que se utiliza para simplificar la creación de objetos","b) Un mecanismo que se utiliza para inyectar dependencias en una clase","c) Una técnica que se utiliza para mejorar el rendimiento de una aplicación"},
+            {"a) Un patrón de diseño que se utiliza para simplificar la creación de objetos","b) Un mecanismo que se utiliza para inyectar dependencias en una clase","c) Una técnica que se utiliza para mejorar el rendimiento de una aplicación"},
+            {"a) Un tipo de dato que se utiliza para representar objetos en Spring", "b) Una clase que se utiliza para acceder a una base de datos", "c) Un objeto que es gestionado por Spring y que puede ser inyectado en otras clases"},
+            {"a) applicationContext.xml", "b) spring-config.xml", "c) web.xml"},
+            {"a) Una base de datos que almacena objetos de Spring","b) Un objeto que se utiliza para gestionar la creación y destrucción de beans en Spring", "c) Una clase que se utiliza para acceder a servicios web"},
+            {"a) Una clase que se utiliza para manejar las solicitudes HTTP de una aplicación web", "b) Un objeto que se utiliza para acceder a una base de datos", "c) Un tipo de vista en Spring MVC",},
+            {"a) Una clase que se utiliza para acceder a una base de datos", "b) Un objeto que se utiliza para gestionar la lógica de negocio de una aplicación", "c) Un tipo de vista en Spring MVC"},
+            {"a) Una clase que se utiliza para manejar las solicitudes HTTP de una aplicación web","b) Un objeto que se utiliza para acceder a una base de datos", "c) Un componente que se encarga de mostrar la información al usuario en una aplicación web"},
+            {"a) Un archivo de configuración que se utiliza para establecer las propiedades de una aplicación Spring Boot", "b) Un archivo de log que registra los eventos de la aplicación Spring Boot", "c) Un archivo que se utiliza para definir los controladores en Spring Boot"},
+            {"a) Una anotación que se utiliza para indicar que un método es un punto de entrada de una aplicación Spring", "b) Una anotación que se utiliza para indicar que un campo o constructor debe ser inyectado por Spring", "c) Una anotación que se utiliza para indicar que un método debe ser ejecutado antes de que se ejecute un método específico"},
+            {"a) Una anotación que se utiliza para indicar que una clase es un controlador en Spring MVC", "b) Una anotación que se utiliza para indicar que un método es un punto de entrada de una aplicación Spring", "c) Una anotación que se utiliza para indicar que un campo o constructor debe ser inyectado por Spring"},
+            {"a) Una anotación que se utiliza para indicar que una clase es un servicio en Spring", "b) Una anotación que se utiliza para indicar que un método es un punto de entrada de una aplicación Spring", "c) Una anotación que se utiliza para indicar que un campo o constructor debe ser inyectado por Spring"},
+            {"a) Una anotación que se utiliza para indicar que una clase es un componente en Spring", "b) Una anotación que se utiliza para indicar que un método es un punto de entrada de una aplicación Spring", "c) Una anotación que se utiliza para indicar que un campo o constructor debe ser inyectado por Spring"},
+            {"a) Una anotación que se utiliza para inyectar un valor en una propiedad de una clase", "b) Una anotación que se utiliza para indicar que un método es un punto de entrada de una aplicación Spring", "c) Una anotación que se utiliza para indicar que un campo o constructor debe ser inyectado por Spring"},
+            {"a) Una anotación que se utiliza para mapear una solicitud HTTP a un método en un controlador en Spring MVC", "b) Una anotación que se utiliza para indicar que una clase es un controlador en Spring MVC", "c) Una anotación que se utiliza para inyectar un valor en una propiedad de una clase"},
+            {"a) El contenedor que se utiliza para ejecutar una aplicación Spring",
+                    "b) El contenedor que se utiliza para administrar los componentes en una aplicación Spring",
+                    "c) El contenedor que se utiliza para establecer la configuración de una aplicación Spring"
+            },
+            {"a) Un objeto que se crea y administra por el contenedor de IoC en Spring",
+                    "b) Un archivo que se utiliza para establecer la configuración de una aplicación Spring",
+                    "c) Un componente que se encarga de mostrar la información al usuario en una aplicación web en Spring"
+            },
+            {"a) Una anotación que se utiliza para indicar que un método debe ser ejecutado en una transacción",
+                    "b) Una anotación que se utiliza para indicar que una clase es un controlador en Spring MVC",
+                    "c) Una anotación que se utiliza para inyectar un valor en una propiedad de una clase en Spring"
+            },
+
+
+    };
+    public static String correctAnswers[]={
+            "b) Un framework de desarrollo de aplicaciones",
+            "a) Un proyecto que se encarga de la configuración inicial de una aplicación Spring",
+            "b) Un mecanismo que se utiliza para inyectar dependencias en una clase",
+            "b) Un mecanismo que se utiliza para inyectar dependencias en una clase",
+            "c) Un objeto que es gestionado por Spring y que puede ser inyectado en otras clases",
+            "a) applicationContext.xml",
+            "b) Un objeto que se utiliza para gestionar la creación y destrucción de beans en Spring",
+            "a) Una clase que se utiliza para manejar las solicitudes HTTP de una aplicación web",
+            "b) Un objeto que se utiliza para gestionar la lógica de negocio de una aplicación",
+            "c) Un componente que se encarga de mostrar la información al usuario en una aplicación web",
+            "a) Un archivo de configuración que se utiliza para establecer las propiedades de una aplicación Spring Boot",
+            "b) Una anotación que se utiliza para indicar que un campo o constructor debe ser inyectado por Spring",
+            "a) Una anotación que se utiliza para indicar que una clase es un controlador en Spring MVC",
+            "a) Una anotación que se utiliza para indicar que una clase es un servicio en Spring",
+            "a) Una anotación que se utiliza para indicar que una clase es un componente en Spring",
+            "a) Una anotación que se utiliza para inyectar un valor en una propiedad de una clase",
+            "a) Una anotación que se utiliza para mapear una solicitud HTTP a un método en un controlador en Spring MVC",
+            "b) El contenedor que se utiliza para administrar los componentes en una aplicación Spring",
+            "a) Un objeto que se crea y administra por el contenedor de IoC en Spring",
+            "a) Una anotación que se utiliza para indicar que un método debe ser ejecutado en una transacción."
+
+    };
+
+
+}
